895b75245cfc3c788bef1fef98516ed87b278069
Isaac R. Sartori Jr <isaac.sartori@tramontina.com>
2021-11-08 07:57:46 -0300

    adicionado exemplo dos printservices

--- Program.cs
+++ Program.cs
@@ -3,17 +3,35 @@ using System.Linq;
 
 namespace curso
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PrinterService ps = new("PS1");
+            PrinterService ps2 = new("PS2");
 
+            var pss = new PaperSupplierService(ps);
+            var tss = new TonerSupplierService(ps);
+            var cs = new CleanerService();
+            cs.Handle(ps);
+            cs.Handle(ps2);
+            //ps.AfterPrinting.Add( (p) => Console.WriteLine("Isaac Ingrato!!!!") ); 
+
+            ps.Print("abc");
+            ps.Print("def");
+            ps.Print("ghi");
+
+            ps2.Print("xyz");
+        }
+
+        static void MostrarUFsEMunicipios()
+        {
             var gMunicipios = new GestorMunicipios();
             var ufs = gMunicipios.ObterUFs().Result.OrderBy(u => u.nome);
             foreach (GestorMunicipios.UF uf in ufs)
             {
-                if (uf.sigla != "RS") {
+                if (uf.sigla != "RS")
+                {
                     Console.WriteLine($"Pulando {uf}...");
                     continue;
                 }
@@ -23,7 +41,7 @@ namespace curso
 
                 foreach (GestorMunicipios.Municipio m in uf.Municipios)
                     Console.WriteLine(m);
-                
+
                 Console.WriteLine($"####################");
             }
         }
